4ab28fd55e372fe04f4e8513088e9574daaaa5c8
moeark-peralta <markperaltaasdgit@gmail.com>
2024-03-04 21:14:43 +0800

    Computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// Computer Choice function
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
