9ffb0ff5c22b1adee63b464aa1c9c427621298c7
moeark-peralta <markperaltaasdgit@gmail.com>
2024-04-01 22:48:21 +0800

    added Play Round function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function getComputerChoice() {
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayChouce();
+// prompt(
+//   "Pick between Rock, Scissors or Paper"
+// ).toLowerCase();
+
+console.log(playerSelection, computerSelection);
+
+// Formatting
+function capitalizeFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function getPlayChouce() {
   const rand = Math.floor(Math.random() * 3) + 1;
-  console.log(rand);
 
   switch (rand) {
     case 1:
@@ -19,4 +31,53 @@ function getComputerChoice() {
   }
 }
 
-getComputerChoice();
+function getComputerChoice() {
+  const rand = Math.floor(Math.random() * 3) + 1;
+
+  switch (rand) {
+    case 1:
+      if (rand === 1) return "rock";
+      break;
+  }
+  switch (rand) {
+    case 2:
+      if (rand === 2) return "paper";
+      break;
+  }
+  switch (rand) {
+    case 3:
+      if (rand === 3) return "scissors";
+      break;
+  }
+}
+
+// Play Round of RSP
+
+function playRound(playerSelection, computerSelection) {
+  // Player Win
+  const capitalizedPlayerSelection = capitalizeFirstLetter(playerSelection);
+  const capitalizedComputerSelection = capitalizeFirstLetter(computerSelection);
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    console.log(
+      `Player: ${capitalizedPlayerSelection} wins against Computer: ${capitalizedComputerSelection}`
+    );
+  }
+  //Computer Win
+  else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    console.log(
+      `Computer: ${capitalizedComputerSelection} wins against Player: ${capitalizedPlayerSelection}`
+    );
+  } else if (playerSelection === computerSelection) {
+    console.log(`It's a Draw!`);
+  }
+}
+
+playRound(playerSelection, computerSelection);
